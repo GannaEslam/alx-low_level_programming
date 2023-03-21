70265b7b8904bba5935e11dc0a9de15f0aeffe43
Ganna <g6800245@gmail.com>
2023-03-21 08:24:32 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,25 +1,14 @@
 #include "main.h"
 /**
- * print_sign- print_alphabet
- *@n : the character
- * Return: 0 or 1
+ * _isalpha - a function that checks for alphabetic character
+ * @c: single letter input
+ * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
  */
-int print_sign(int n)
+int _isalpha(int c)
 {
-if (n == 0)
-{
-_putchar('0');
-return (0);
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
 }
-else if (n != 0 && n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if (n < 0)
-{
-_putchar('-');
-return (-1);
-}
-return (0);
-}
+
