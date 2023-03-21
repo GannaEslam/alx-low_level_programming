a4524cdb0edc43bc30134761f2d6dd2743c501ce
Ganna <g6800245@gmail.com>
2023-03-21 08:15:25 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,18 @@
 #include "main.h"
-
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _islower- print_alphabet
+ *@c : the character
+ * Return: 0 or 1
  */
-int main(void)
+int _islower(int c)
 {
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+if (c >= 65 && c <= 90)
+{
+return (0);
+}
+else if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
 }
